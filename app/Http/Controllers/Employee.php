bbbc4cea2414b6e321f4d3feef8e0fa6efae8beb
Aldi Unanto <aldiunanto@yahoo.com>
2017-08-31 09:06:49 +0700

    Solved.

--- app/Http/Controllers/Employee.php
+++ app/Http/Controllers/Employee.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 class Employee extends Controller {
 
     public function index(){
-    	/*$str = '{"em_nik":"1305007","em_name":"Kartika","em_div":"Marketing"}';
-    	$turn = json_decode($str);
-
-    	echo $turn->em_nik;*/
-
     	$fetch = Employees::all();
     	return response()->json($fetch)->header('Access-Control-Allow-Origin', '*');
     }
@@ -21,13 +16,8 @@ class Employee extends Controller {
     	return response()->json($row)->header('Access-Control-Allow-Origin', '*');
     }
     public function store(Request $req){
-    	foreach($req->all() as $key => $val){
-    		$param = $key;
-    	}
-
-    	$turn = json_decode($param);
-
-    	return response()->json($turn)->header('Access-Control-Allow-Origin', '*');
+    	$output = json_decode($req->input('params'));
+    	return response()->json($output)->header('Access-Control-Allow-Origin', '*');
     }
 
 }
